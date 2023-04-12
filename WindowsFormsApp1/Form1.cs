ca04027a0b58d5b0bb847d19e7f1ec3ed7f2627e
ZXL0ver <130565880+ZXL0ver@users.noreply.github.com>
2023-04-12 21:22:16 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            _firstService = new BankTransferService();
+            var creditCardService = new CreditCardService();
+            var payPalService = new PayPalService();
+
+            _firstService.SetNext(creditCardService).SetNext(payPalService);
+        }
+
+        private PaymentService _firstService;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int amount))
+            {
+                MessageBox.Show("Неправильна сума платежу");
+                return;
+            }
+            _firstService.Handle(amount);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(button1,null);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            {
+                if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+                {
+
+                    return;
+                }
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/PaymentService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public abstract class PaymentService
+{
+    protected PaymentService _nextService;
+
+    public PaymentService SetNext(PaymentService service)
+    {
+        _nextService = service;
+        return _nextService;
+    }
+
+    public abstract bool Handle(int amount);
+}
+
+public class BankTransferService : PaymentService
+{
+    public override bool Handle(int amount)
+    {
+        if (amount <= 500)
+        {
+            MessageBox.Show("Оплачується банківським переказом");
+            return true;
+        }
+        else if (_nextService != null)
+        {
+            MessageBox.Show("Сума перевищує ліміт банківського переказу. Спробувати наступну послугу...");
+            return _nextService.Handle(amount);
+        }
+        else
+        {
+            MessageBox.Show("Платіж не може бути завершений");
+            return false;
+        }
+    }
+}
+
+public class CreditCardService : PaymentService
+{
+    public override bool Handle(int amount)
+    {
+        if (amount <= 1000)
+        {
+            MessageBox.Show("Оплачено кредитною карткою");
+            return true;
+        }
+        else if (_nextService != null)
+        {
+            MessageBox.Show("Сума перевищує ліміт кредитної картки. Спробувати наступну послугу...");
+            return _nextService.Handle(amount);
+        }
+        else
+        {
+            MessageBox.Show("Платіж не може бути завершений");
+            return false;
+        }
+    }
+}
+
+public class PayPalService : PaymentService
+{
+    public override bool Handle(int amount)
+    {
+        if (amount <= 2000)
+        {
+            MessageBox.Show("Оплачено за допомогою PayPal");
+            return true;
+        }
+        else if (_nextService != null)
+        {
+            MessageBox.Show("Сума перевищує ліміт PayPal. Спробуємо наступну послугу...");
+            return _nextService.Handle(amount);
+        }
+        else
+        {
+            MessageBox.Show("Платіж не може бути завершений");
+            return false;
+        }
+    }
+}
+
+
